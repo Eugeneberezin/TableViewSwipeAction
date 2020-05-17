cf641143711940fdb4dafa1488fa362b8388fbd8
Eugene Berezin <berezin.nlp@gmail.com>
2020-05-16 18:40:01 -0700

    finished project

--- SwipeAction/SwipeAction/ViewController.swift
+++ SwipeAction/SwipeAction/ViewController.swift
@@ -18,6 +18,40 @@ class ViewController: UITableViewController {
         tableView.tableFooterView = UIView()
     }
     
+    var numberOfRows = 3
+    private func delete(rowIndexPathAt indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (_, _, _) in
+            guard let self = self else {return}
+            
+            self.numberOfRows -= 1
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            self.tableView.reloadData()
+        }
+        
+       
+        
+        return action
+    }
+    
+    private func edit(rowIndexPathAt indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .normal, title: "Edit") { [weak self] (_, _, _) in
+            
+            let alert = UIAlertController(title: "Do you want to edit? ", message: "You could do so when we implement this functionality", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self?.present(alert, animated: true)
+        }
+        
+        return action
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let edit = self.edit(rowIndexPathAt: indexPath)
+        let delete = self.delete(rowIndexPathAt: indexPath)
+        let swipe = UISwipeActionsConfiguration(actions: [delete, edit])
+        return swipe
+    }
+    
+  
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
@@ -31,7 +65,7 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return numberOfRows
     }
 
 
