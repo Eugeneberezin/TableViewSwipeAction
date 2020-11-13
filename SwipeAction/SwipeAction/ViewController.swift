1731550a716deceea69ed3b928c2f4c4b81ebf14
Eugene Berezin <berezin.nlp@gmail.com>
2020-11-12 16:28:47 -0800

    commit

--- SwipeAction/SwipeAction/ViewController.swift
+++ SwipeAction/SwipeAction/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(TextFieldTableViewCell.self, forCellReuseIdentifier: "text")
         tableView.backgroundColor = .systemGray5
         title = "Swipe Action!"
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -33,31 +34,25 @@ class ViewController: UITableViewController {
         return action
     }
     
-    private func edit(rowIndexPathAt indexPath: IndexPath) -> UIContextualAction {
-        let action = UIContextualAction(style: .normal, title: "Edit") { [weak self] (_, _, _) in
-            
-            let alert = UIAlertController(title: "Do you want to edit? ", message: "You could do so when we implement this functionality", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default))
-            self?.present(alert, animated: true)
-        }
-        
-        return action
-    }
-    
-    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let edit = self.edit(rowIndexPathAt: indexPath)
-        let delete = self.delete(rowIndexPathAt: indexPath)
-        let swipe = UISwipeActionsConfiguration(actions: [delete, edit])
-        return swipe
-    }
-    
-  
-    
+//    private func edit(rowIndexPathAt indexPath: IndexPath) -> UIContextualAction {
+//        let action = UIContextualAction(style: .normal, title: "Edit") { [weak self] (_, _, _) in
+//
+//            let alert = UIAlertController(title: "Do you want to edit? ", message: "You could do so when we implement this functionality", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "OK", style: .default))
+//            self?.present(alert, animated: true)
+//        }
+//
+//        return action
+//    }
+//
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Swipe me!"
-        cell.backgroundColor = .systemTeal
+        let cell = tableView.dequeueReusableCell(withIdentifier: "text", for: indexPath) as! TextFieldTableViewCell
+      
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
